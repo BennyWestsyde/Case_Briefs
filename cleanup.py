ba76ca02569aede142fe535aecdafa19cfc10f99
BennyWestsyde <bennett.westfall@utdallas.edu>
2025-08-13 10:25:39 -0500

    Created a cleanup script to remove temp files

---
+++ cleanup.py
@@ -0,0 +1,15 @@
+import os
+
+def clean_dir(path:str):
+    for file in os.listdir(path):
+        curr_path = os.path.join(path,file)
+        if os.path.isfile(curr_path):
+            if file.endswith(("aux","fdb_latexmk","fls","idx","ilg","ind","log","out","synctex.gz","synctex(busy)","toc")):
+                os.remove(curr_path)
+        elif os.path.isdir(curr_path):
+            clean_dir(curr_path)
+
+if __name__ == "__main__":
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    clean_dir(curr_path)
+    
