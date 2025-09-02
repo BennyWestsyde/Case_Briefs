7c19069c86183ef8b6db2884ed1719b2ffa3cb7a
Bennett Westfall <bennett@bennettshome.com>
2025-09-02 13:43:35 -0500

    Formatted cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,14 +1,30 @@
 import os
 from logger import StructuredLogger
-log = StructuredLogger("Cleanup","TRACE","CaseBriefs.log",True,None,True,True)
 
-def clean_dir(path:str):
+log = StructuredLogger("Cleanup", "TRACE", "CaseBriefs.log", True, None, True, True)
+
+
+def clean_dir(path: str):
     log.info(f"Cleaning directory: {path}")
     for file in os.listdir(path):
-        curr_path = os.path.join(path,file)
+        curr_path = os.path.join(path, file)
         if os.path.isfile(curr_path):
             log.trace(f"Found file: {curr_path}")
-            if file.endswith(("aux","fdb_latexmk","fls","idx","ilg","ind","log","out","synctex.gz","synctex(busy)","toc")):
+            if file.endswith(
+                (
+                    "aux",
+                    "fdb_latexmk",
+                    "fls",
+                    "idx",
+                    "ilg",
+                    "ind",
+                    "log",
+                    "out",
+                    "synctex.gz",
+                    "synctex(busy)",
+                    "toc",
+                )
+            ):
                 log.trace(f"File in glob for deletion")
                 log.debug(f"Removing file: {curr_path}")
                 os.remove(curr_path)
@@ -16,7 +32,7 @@ def clean_dir(path:str):
             log.debug(f"Pivoting to directory: {curr_path}")
             clean_dir(curr_path)
 
+
 if __name__ == "__main__":
-    curr_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..")
+    curr_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
     clean_dir(curr_path)
-    
